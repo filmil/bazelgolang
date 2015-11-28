213007e62f6605a8d0c685f87993ace37fd5e276
Filip Filmar <filmil@gmail.com>
2015-11-27 23:54:43 -0800

    Adds an example unit test that reads some information from an external resource file.
    Try it out by issuing:
    
    ```bash
    bazel test //lib/...
    ```
    
    Resource file access is the current difference between a bazel-built
    golang source code and test built with 'go test'.
    
    If you wanted to access the file when testing with 'go test', you would
    have to open as:
    
    ```go
    file, err := os.Open("file.txt)
    ```
    
    With bazel it will have to be:
    
    ```go
    file, err := os.Open("lib/file.txt)
    ```

---
+++ lib/file_test.go
@@ -0,0 +1,23 @@
+package lib
+
+import (
+	"bufio"
+	"os"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	// The data file is put into the directory relative to the *runfiles
+	// dir, in a directory corresponding to the package name.
+	file, err := os.Open("lib/file.txt")
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if text := scanner.Text(); text != SayHello() {
+			t.Errorf("Mismatch: '%v'", text)
+		}
+	}
+
+}
