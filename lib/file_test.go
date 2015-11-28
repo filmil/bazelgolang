0007f60e1bcd0874f228337605356763fa8b7bb4
Filip Filmar <filmil@gmail.com>
2015-11-28 01:17:11 -0800

    Proposes a way to work around the differences between resource file references for 'go test' and 'bazel test'.

--- lib/file_test.go
+++ lib/file_test.go
@@ -6,10 +6,19 @@ import (
 	"testing"
 )
 
+// Returns the directory relative to the directory the test is running in where
+// resource files can be found.
+func dataDir(dirname string) string {
+	if len(os.Getenv("TEST_TMPDIR")) > 0 {
+		return dirname
+	}
+	return "."
+}
+
 func TestHello(t *testing.T) {
 	// The data file is put into the directory relative to the *runfiles
 	// dir, in a directory corresponding to the package name.
-	file, err := os.Open("lib/file.txt")
+	file, err := os.Open(dataDir("lib") + "/file.txt")
 	if err != nil {
 		t.Errorf("Error: %v", err)
 	}
